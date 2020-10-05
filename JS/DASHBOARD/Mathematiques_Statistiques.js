b185e90d1e33a998c1e3183fa8ca2250a52f32cf
viviennoel <52512132+viviennoel@users.noreply.github.com>
2020-10-05 11:31:16 +0200

    Update Mathematiques_Statistiques.js

--- JS/DASHBOARD/Mathematiques_Statistiques.js
+++ JS/DASHBOARD/Mathematiques_Statistiques.js
@@ -29,27 +29,14 @@ function Display_Evaluation_Diagnostic() {
 `
 <!-- NOTION DE FONCTION -->
                             <div class="col-12" data-aos="fade-up" data-aos-easing="ease-out" data-aos-duration="2000">
-                                <h2 class="pt-5 pb-5">Exploiter la représentation d'une fonction</h2>
-                                <p class="text-justify">Au cours de cette étude, nous nous intéresserons particulièrement à la sécurité routière et à la manière d'utiliser sa voiture de manière responsable. Une étude mathématique de différents problèmes nous aidera à y voir plus clair.</p>
-                                <p class="text-justify">Vous testerez au cours de ce test vos compétences concernant :</p>
+                                <h2 class="pt-5 pb-5">La notion de probabilité et de proportionnalité</h2>
+                                <p class="text-justify">Avant de cpommencer ce chapitre, il est essentiel d'avoir acquis les notions de base concernant la proportionnalité et le produit en croix. Une évaluation diagnostique est mise en place sous forme de quizz en classe.</p>
+                                <p class="text-justify">Afin de pouvoir suivre ce cours au mieux, il est essentiel d'avoir acquis les notions suivantes. Une remédiation sera mise en place dans le cas contraire :</p>
                                 <ol>
-                                    <li>L'exploitation de la représentation graphique d'une fonction</li>
-                                    <li>L'utilisation et la description des variations d'une fonction</li>
-                                    <li>L'utilisation d'un tableur et d'une calculette en vue d'étudier l'évolution d'une fonction et de résoudre un problème.</li>
+                                    <li>La notion de proportionnalité</li>
+                                    <li>La notion de fraction</li>
+                                    <li>Le calcul du produit en croix</li>
                                 </ol>
-
-
-                                <!-- PREMIERE PARTIE - REPRESENTATION GRAPHIQUE -->
-                                <h3 class="pt-5">C'est le moment de tester vos connaissances!</h3>
-                                <div class="row pt-5 pb-5">
-                                    <div class="col-md-6" id="Arrow_right">
-                                        <img src="../../IMAGES/TEACHING/Arrow_right.png" alt="Here Evaluation"/>
-                                    </div>
-                                    <div class="Evaluation_File col-md-6 m-auto">
-                                        <a href="Evaluation_Fonctions_graph.pdf" download="FONCTION - Evaluation_diagnostique_représentation_graphique"><p>Télécharger l'évaluation : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
-                                        <a href="Evaluation_correction_Fonctions_graph.pdf" download="FONCTION_Evaluation_diagnostique_graphique_correction"><p>Télécharger la correction : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
-                                    </div>
-                                </div>
                             </div>
 `
     Hide_all();
@@ -66,25 +53,25 @@ function Display_Cours() {
         `
 <!-- NOTION DE FONCTION -->
                             <div class="col-12 cours" data-aos="fade-up" data-aos-easing="ease-out" data-aos-duration="2000">
-                                <h2 class="pt-5 pb-5 black">Les fonctions usuelles</h2>
-                                <p class="text-justify">Cette première partie de cours nous permettra de comprendre et d'apprendre à utiliser les fonctions usuelles pour résoudres des problèmes de la vie courante et professionnelle.</p>
+                                <h2 class="pt-5 pb-5 black">Les probabilités</h2>
+                                <p class="text-justify">Ce cours nous permettra d'approfondir les notions de calculs de probabilité abordés en troisième. Cette étude s'appuiera sur des cas concrêts et qui nous permettront également d'aborder la notion de probabilité conditionnelle.</p>
                                 <p class="text-justify">Les compétences travaillées dans cette partie du cours sont :</p>
                                 <ol>
-                                    <li>L'exploitation de la représentation graphique d'une fonction usuelle</li>
-                                    <li>L'utilisation et la description des variations d'une fonction usuelle</li>
-                                    <li>L'application de la notion de correlation entre deux variables et son utilisation.</li>
+                                    <li>L'exploitation des informations dans le cadre du calcul de probabilité</li>
+                                    <li>La communication des résultats et des informations relatives à la résolution d'un problème probabiliste.</li>
+                                    <li>Le calcul de probabilités conditionnelles.</li>
                                 </ol>
 
 <div class="part_cours mt-5">
                                 <!-- PREMIERE PARTIE - La fonction affine -->
-                                <h2 class="pt-5">Fonction affine</h2>
+                                <h2 class="pt-5">Calculs de probabilité</h2>
                                 <h3 class="pt-5">Téléchargez le powerpoint utilisé en cours ci dessous</h3>
                                 <div class="row pt-5 pb-5">
                                     <div class="col-md-6" id="Arrow_right">
                                         <img src="../../IMAGES/TEACHING/Arrow_right.png" alt="Here Evaluation"/>
                                     </div>
                                     <div class="Evaluation_File col-md-6 m-auto">
-                                        <a href="Evaluation_Fonctions_graph.pdf" download="Cours - Les fonctions usuelles"><p>Télécharger le diaporama : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
+                                        <a href="../../PROBABILITES/Evaluation_Fonctions_graph.pdf" download="Cours - Les fonctions usuelles"><p>Télécharger le diaporama : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
                                     </div>
 
                                     <div class="col-12 blanc">
@@ -92,65 +79,7 @@ function Display_Cours() {
                                         <p>Les notions clées abordées dans ce chapitre</p>
                                     </div>
                                 </div>
-</div>
-
-<div class="part_cours mt-5">
-                                <!-- PREMIERE PARTIE - La fonction affine -->
-                                <h2 class="pt-5">Fonction polynome</h2>
-                                <h3 class="pt-5">Téléchargez le powerpoint utilisé en cours ci dessous</h3>
-                                <div class="row pt-5 pb-5">
-                                    <div class="col-md-6" id="Arrow_right">
-                                        <img src="../../IMAGES/TEACHING/Arrow_right.png" alt="Here Evaluation"/>
-                                    </div>
-                                    <div class="Evaluation_File col-md-6 m-auto">
-                                        <a href="Evaluation_Fonctions_graph.pdf" download="Cours - Les fonctions usuelles"><p>Télécharger le diaporama : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
-                                    </div>
-
-                                    <div class="col-12 blanc">
-                                        <h3>Les notions clés</h3>
-                                        <p>Les notions clées abordées dans ce chapitre</p>
-                                    </div>
-                                </div>
-</div>
-
-<div class="part_cours mt-5">
-                                <!-- Troisième PARTIE - La fonction Exponentielle -->
-                                <h2 class="pt-5">Fonction Exponentielle</h2>
-                                <h3 class="pt-5">Téléchargez le document utilisé en cours ci dessous</h3>
-                                <div class="row pt-5 pb-5">
-                                    <div class="col-md-6" id="Arrow_right">
-                                        <img src="../../IMAGES/TEACHING/Arrow_right.png" alt="Here Evaluation"/>
-                                    </div>
-                                    <div class="Evaluation_File col-md-6 m-auto">
-                                        <a href="Evaluation_Fonctions_graph.pdf" download="Cours - Les fonctions usuelles"><p>Télécharger le diaporama : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
-                                    </div>
-
-                                    <div class="col-12 blanc">
-                                        <h3>Les notions clés</h3>
-                                        <p>Les notions clées abordées dans ce chapitre</p>
-                                    </div>
-                                </div>
-</div>
-
-<div class="part_cours mt-5 mb-5">
-                                <!-- Deuxième PARTIE - La fonction Logarithme -->
-                                <h2 class="pt-5">Fonction Logarithme</h2>
-                                <h3 class="pt-5">Téléchargez le document utilisé en cours ci dessous</h3>
-                                <div class="row pt-5 pb-5">
-                                    <div class="col-md-6" id="Arrow_right">
-                                        <img src="../../IMAGES/TEACHING/Arrow_right.png" alt="Here Evaluation"/>
-                                    </div>
-                                    <div class="Evaluation_File col-md-6 m-auto">
-                                        <a href="Evaluation_Fonctions_graph.pdf" download="Cours - Les fonctions usuelles"><p>Télécharger le diaporama : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
-                                    </div>
-
-                                    <div class="col-12 blanc">
-                                        <h3>Les notions clés</h3>
-                                        <p>Les notions clées abordées dans ce chapitre</p>
-                                    </div>
-                                </div>
-                            </div>
-</div>`
+                            </div>`
     Hide_all();
     document.getElementById('Cours').style.display = 'flex';
     document.getElementById('Cours').innerHTML = content;
@@ -166,7 +95,7 @@ function Display_Exercices() {
 <!-- NOTION DE FONCTION -->
                             <div class="col-12" data-aos="fade-up" data-aos-easing="ease-out" data-aos-duration="2000">
                                 <h2 class="pt-5">Les exercices pour vous permettre de monter en compétence</h2>
-                                <p class="text-justify">Un document présente une série d'exercices intéressants pour une montée en compétence dans le domaine des études de fonctions.</p>
+                                <p class="text-justify">Un document présente une série d'exercices intéressants pour une montée en compétence dans le domaine des calculs de probabilité.</p>
                                 <p class="text-justify">Les exercices "facultatifs" sont à faire sur la base du volontariat et à rendre directement au professeur pour correction.</p>
                                 <p class="text-justify">Les exercices "Avancés" sont à faire sur la base du volontariat également et permettent d'explorer plus avant les notions mathématiques abordées.</p>
                    
@@ -178,7 +107,7 @@ function Display_Exercices() {
                                         <img src="../../IMAGES/TEACHING/Arrow_right.png" alt="Here Evaluation"/>
                                     </div>
                                     <div class="Evaluation_File col-md-6 m-auto">
-                                        <a href="Evaluation_Fonctions_graph.pdf" download="Cours - Les fonctions usuelles"><p>Télécharger les exercices : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
+                                        <a href="Evaluation_Fonctions_graph.pdf" download="Exercices - Les probabilités"><p>Télécharger les exercices : <img src="../../IMAGES/TEACHING/Download.png" alt="Evaluation Diagnostique" /></p></a>
                                     </div>
                                 </div>
                             </div>
